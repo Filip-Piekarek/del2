3c74f3f3b554cbe4da7066fafce33a2e0e49eedd
Omar <omar.hussain@mail.mcgill.ca>
2020-10-04 12:39:18 -0400

    Created Spring Project

---
+++ ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/ArtGalleryApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.artgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtGalleryApplication.class, args);
+	}
+
+}
