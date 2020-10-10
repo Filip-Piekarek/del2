bcad25bb27ea58e05029bfa8d1fa8b88d29c4d46
Patrick Juneau <patrickjuneau7@gmail.com>
2020-10-09 21:06:37 -0400

    Test user,artist,client working

--- ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/model/Artist.java
+++ ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/model/Artist.java
@@ -3,6 +3,7 @@ package ca.mcgill.ecse321.artgallery.model;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -13,38 +14,38 @@ import javax.persistence.Table;
 @Entity
 @Table(name="artists")
 public class Artist extends UserRole{
-	
-	@Id
-	@Column(unique=true)
-	private long id;
+    
+    @Id
+    @Column(unique=true)
+    private long id;
 
-	public void setId(long value) {
-		this.id = value;
-	}
-	@Id
-	public long getId() {
-		return this.id;
-	}
+    public void setId(long value) {
+        this.id = value;
+    }
+    @Id
+    public long getId() {
+        return this.id;
+    }
 
-	private Set<Artwork> artworks;
+    private Set<Artwork> artworks;
 
-	@OneToMany(mappedBy="creator" )
-	public Set<Artwork> getArtworks() {
-		return this.artworks;
-	}
+    @OneToMany(mappedBy="creator", cascade={CascadeType.ALL})
+    public Set<Artwork> getArtworks() {
+        return this.artworks;
+    }
 
-	public void setArtworks(Set<Artwork> artworkss) {
-		this.artworks = artworkss;
-	}
+    public void setArtworks(Set<Artwork> artworkss) {
+        this.artworks = artworkss;
+    }
 
-	@Column
-	private String biography;
+    @Column
+    private String biography;
 
-	public void setBiography(String value) {
-		this.biography = value;
-	}
-	
-	public String getBiography() {
-		return this.biography;
-	}
+    public void setBiography(String value) {
+        this.biography = value;
+    }
+    
+    public String getBiography() {
+        return this.biography;
+    }
 }
--- ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/model/Artwork.java
+++ ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/model/Artwork.java
@@ -3,6 +3,8 @@ import javax.persistence.Id;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -11,87 +13,88 @@ import java.sql.Date;
 @Entity
 @Table(name="artworks")
 public class Artwork{
-	
-	@Column
-	private Artist creator;
-	
-	@OneToOne(optional=false)
-	public Artist getCreator() {
-		return this.creator;
-	}
+    
+    @Column
+    private Artist creator;
+    
+    @OneToOne(optional=false)
+    public Artist getCreator() {
+        return this.creator;
+    }
 
-	public void setCreator(Artist creator) {
-		this.creator = creator;
-	}
-	
-	private Posting posting;
+    public void setCreator(Artist creator) {
+        this.creator = creator;
+    }
+    
+    private Posting posting;
 
-	@OneToOne(mappedBy="item" , optional=false)
-	public Posting getPosting() {
-		return this.posting;
-	}
+    @OneToOne(mappedBy="item" , optional=true)
+    public Posting getPosting() {
+        return this.posting;
+    }
 
-	public void setPosting(Posting posting) {
-		this.posting = posting;
-	}
-	
+    public void setPosting(Posting posting) {
+        this.posting = posting;
+    }
+    
 
-	@Column
-	private String name;
+    @Column
+    private String name;
 
-	public void setName(String value) {
-		this.name = value;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	@Column
-	private String description;
+    public void setName(String value) {
+        this.name = value;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    @Column
+    private String description;
 
-	public void setDescription(String value) {
-		this.description = value;
-	}
-	
-	public String getDescription() {
-		return this.description;
-	}
-	
-	
-	@Id
-	@Column(unique=true)
-	private long id;
+    public void setDescription(String value) {
+        this.description = value;
+    }
+    
+    public String getDescription() {
+        return this.description;
+    }
+    
+    
+    @Id
+    @Column(unique=true)
+    private long id;
 
-	public void setId(long value) {
-		this.id = value;
-	}
-	
-	@Id
-	public long getId() {
-		return this.id;
-	}
-	
-	
-	@Enumerated
-	private ArtworkType artworkType;
+    public void setId(long value) {
+        this.id = value;
+    }
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return this.id;
+    }
+    
+    
+    @Enumerated
+    private ArtworkType artworkType;
 
-	public void setArtworkType(ArtworkType value) {
-		this.artworkType = value;
-	}
-	
-	public ArtworkType getArtworkType() {
-		return this.artworkType;
-	}
-	
-	@Column
-	private Date date;
+    public void setArtworkType(ArtworkType value) {
+        this.artworkType = value;
+    }
+    
+    public ArtworkType getArtworkType() {
+        return this.artworkType;
+    }
+    
+    @Column
+    private Date date;
 
-	public void setDate(Date value) {
-		this.date = value;
-	}
-	
-	public Date getDate() {
-		return this.date;
-	}
+    public void setDate(Date value) {
+        this.date = value;
+    }
+    
+    public Date getDate() {
+        return this.date;
+    }
 }
