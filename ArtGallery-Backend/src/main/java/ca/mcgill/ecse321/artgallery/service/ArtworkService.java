f3d254b308410773b4867e3b6acec6495cb1079f
Omar <omar.hussain@mail.mcgill.ca>
2020-10-26 17:26:14 -0400

    Update ArtworkService.java

--- ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/service/ArtworkService.java
+++ ArtGallery-Backend/src/main/java/ca/mcgill/ecse321/artgallery/service/ArtworkService.java
@@ -1,8 +1,13 @@
 package ca.mcgill.ecse321.artgallery.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ca.mcgill.ecse321.artgallery.dao.ArtworkCrudRepository;
 
 @Service
 public class ArtworkService {
+	@Autowired
+	private ArtworkCrudRepository artworkRepo;
 
 }
